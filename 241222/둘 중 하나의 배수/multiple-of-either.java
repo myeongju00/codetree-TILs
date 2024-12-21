7f1925411310a6a4169038658824f6e365b56474
myeongju00 <j51677767@gmail.com>
2024-12-22 00:58:20 +0700

    Title: 둘 중 하나의 배수, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241222/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+
+        if (a % 3 == 0 || a % 5 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        
+    }
+}
