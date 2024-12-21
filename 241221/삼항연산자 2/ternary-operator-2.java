e59894488b1ce7e4dcc7fbe566f1836299cf6b25
myeongju00 <j51677767@gmail.com>
2024-12-21 15:47:59 +0700

    Title: 삼항연산자 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241221/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+
+        if(a == 1) {
+            System.out.println("t");
+        } else {
+            System.out.println("f");
+        }
+
+    }
+}
