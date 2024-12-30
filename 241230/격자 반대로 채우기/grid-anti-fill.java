b1c95af5807f6158d80f5d3d79b41524dd6b5a7b
myeongju00 <j51677767@gmail.com>
2024-12-30 15:54:19 +0700

    Title: 격자 반대로 채우기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241230/격자 반대로 채우기/grid-anti-fill.java
+++ 241230/격자 반대로 채우기/grid-anti-fill.java
@@ -1,40 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
         
+        int n = Integer.parseInt(br.readLine());
         int[][] board = new int[n][n];
-        
-        StringBuilder sb = new StringBuilder();
-
         int num = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if(i % 2 == 1) {
-                for(int j = n - 1; j >= 0; j--) {
+
+        for (int j = n - 1; j >= 0; j--) {
+            if ((n - j) % 2 == 1) {
+                for (int i = n - 1; i >= 0; i--) {
                     board[i][j] = num++;
                 }
             } else {
-                for(int j = 0; j < n; j++) {
+                for (int i = 0; i < n; i++) {
                     board[i][j] = num++;
                 }
             }
-            
         }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                sb.append(board[j][i]).append(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(board[i][j]).append(" ");
             }
             sb.append("\n");
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
